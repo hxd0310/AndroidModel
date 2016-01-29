09a265a7b9da68f397bd8c14ef4ec77279961d57
huxiangduo <huxiangduo@meituan.com>
2016-01-29 17:29:44 +0800

    Build TabViews

---
+++ app/src/main/java/com/bry/ankle/androidmodel/Activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.bry.ankle.androidmodel.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bry.ankle.androidmodel.R;
+import com.bry.ankle.androidmodel.Widget.TabViews;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
---
+++ app/src/main/java/com/bry/ankle/androidmodel/Widget/TabViews.java
@@ -0,0 +1,119 @@
+package com.bry.ankle.androidmodel.Widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Tab导航控件
+ * Created by ankle on 16/1/29.
+ */
+@SuppressWarnings("unused")
+public class TabViews extends LinearLayout {
+
+    private Context mContext;
+
+    private List<View> mViews;
+
+    private OnTabClickListener onTabClickListener;
+
+    public TabViews(Context context) {
+        this(context, null);
+    }
+
+    public TabViews(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TabViews(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mContext = context;
+    }
+
+    private void initViews(int size) {
+        if(null == mViews) {
+            mViews = new ArrayList<>(size);
+        } else {
+            mViews.clear();
+        }
+    }
+
+    private void addViews() {
+        LinearLayout.LayoutParams params;
+        switch (getOrientation()) {
+            case HORIZONTAL:
+                params = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+                break;
+            case VERTICAL:
+                params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 0);
+                break;
+            default:
+                params = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+                break;
+        }
+        params.weight = 1;
+        for(int i = 0; i < mViews.size(); i++) {
+            View view = mViews.get(i);
+            view.setTag(i);
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(null !=  onTabClickListener) {
+                        onTabClickListener.onClickTab(v, (int) v.getTag());
+                    }
+                }
+            });
+            addView(view, params);
+        }
+    }
+
+    public void addViews(int... resIds) {
+        if(resIds.length <=0) {
+            return;
+        }
+        initViews(resIds.length);
+        for(int id : resIds) {
+            View view = LayoutInflater.from(mContext).inflate(id, this, true);
+            mViews.add(view);
+        }
+        addViews();
+    }
+
+    public void addViews(View... views) {
+        if(views.length <=0) {
+            return;
+        }
+        initViews(views.length);
+        Collections.addAll(mViews, views);
+        addViews();
+    }
+
+    public void addViews(List<View> views) {
+        if(null == views || views.size() <=0) {
+            return;
+        }
+        initViews(views.size());
+        mViews.addAll(views);
+        addViews();
+    }
+
+    public void setOnTabClickListener (OnTabClickListener onTabClickListener) {
+        this.onTabClickListener = onTabClickListener;
+    }
+
+    public interface OnTabClickListener {
+        void onClickTab(View v, int position);
+    }
+
+}
